d77a1749bed05d17eb09219fa951279de5a114a2
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-05 22:22:34 +0400

    feat: add routing to the project with 6 pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Pages/About/About";
+import Contact from "./Pages/Contact/Contact";
+import Home from "./Pages/Home/Home";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Resume from "./Pages/Resume/Resume";
+import Skills from "./Pages/Skills/Skills";
 
 /* npm install react-router-dom sass react-vertical-timeline-component rc-progress react-tsparticles tsparticles react-simple-animate
  */
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* particles js */}
+
+      {/* navbar components */}
+
+      {/* main page content */}
+
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
