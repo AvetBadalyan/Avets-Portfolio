4afb71e1173d81cfd873c0889a366c3616d34f68
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-05 22:04:33 +0400

    feat: create structure and install dependancies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import "./App.css";
+
+/* npm install react-router-dom sass react-vertical-timeline-component rc-progress react-tsparticles tsparticles react-simple-animate
+ */
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
