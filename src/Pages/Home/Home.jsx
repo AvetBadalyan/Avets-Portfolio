498422ec164db2bd6d98c484553d54bb1418afec
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-08 23:34:10 +0400

    feat: add buttons and icons to home page with animation

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Home.scss";
+import { FaLinkedin, FaFacebook } from "react-icons/fa";
+import { Animate } from "react-simple-animate";
 
 export default function Home() {
   return (
@@ -10,6 +12,29 @@ export default function Home() {
           Front End Developer 💻
         </h1>
       </div>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{
+          transform: "translateY(600px)",
+        }}
+        end={{
+          transform: "translateX(0px)",
+        }}
+      >
+        <div className="contact-me">
+          <div className="contact-me-buttons-wrapper">
+            <button>Hire Me</button>
+            <a href="">Download Resume</a>
+          </div>
+          <div className="contact-me-social-wrapper">
+            <span>Social Media Links</span>
+            <FaLinkedin size={32} />
+            <FaFacebook size={32} />
+          </div>
+        </div>
+      </Animate>
     </section>
   );
 }
