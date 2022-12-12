5df9f723ee894cf57274f2955d18f83c8ea90bfa
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-12 19:48:19 +0400

    feat: personal data in about section

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import PageHeader from "../../Components/PageHeaderContent/PageHeader";
+import { personalData } from "./utils";
+import "./About.styles.scss";
 
 export default function About() {
   return (
     <div className="about" id="about">
       <PageHeader headerText="About me" />
+      <div className="about-content">
+        <div className="about-content-personalWrapper">
+          <h3 className="developer-content">Front End Developer</h3>
+          <p>About Me section</p>
+          <h3 className="personalContent">Personal Information</h3>
+          <div className="personal-data-container">
+            {personalData.map((item, key) => (
+              <div className="personal-data" key={key}>
+                <span className="title">{item.label}</span>
+                <span className="value">{item.value}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="about-content-serviceWrapper">service wrapper</div>
+      </div>
     </div>
   );
 }
---
+++ src/Pages/About/utils.js
@@ -0,0 +1,22 @@
+export const personalData = [
+  {
+    label: "Name",
+    value: "Avet Badalyan",
+  },
+  {
+    label: "Age",
+    value: "27",
+  },
+  {
+    label: "Address",
+    value: "Yerevan, Armenia",
+  },
+  {
+    label: "Email",
+    value: "avetbadalyan@gmail.com",
+  },
+  {
+    label: "Contact Number",
+    value: "+374-55-280-444",
+  },
+];
