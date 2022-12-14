f169c72fa14cb2808ae3f50ee412f72d543d5c45
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-14 14:07:44 +0400

    feat: add skills data and show them with rc-progress linebar

--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -1,5 +1,37 @@
-import React from 'react'
+import React from "react";
+import PageHeader from "./../../Components/PageHeaderContent/PageHeader";
+import { skillsData } from "./utils";
+import { Line } from "rc-progress";
 
 export default function Skills() {
-  return <div>Skills</div>;
+  return (
+    <div>
+      <div id="skills" className="skills">
+        <PageHeader headerText="My Skills" />
+        <div className="skills-content-wrapper">
+          {skillsData.map((item, key) => (
+            <div key={key} className="skills-content-wrapper-inner-content">
+              <h3 className="skills-content-wrapper-inner-content-category-text">
+                {item.label}
+              </h3>
+              <div>
+                {item.data.map((dataItem, index) => (
+                  <div className="progressbar-wrapper" key={index}>
+                    <p>{dataItem.skillName}</p>
+                    <Line
+                      percent={dataItem.percentage}
+                      strokeWidth="2"
+                      strokeColor="var(--selected-theme-main-color)"
+                      strokeLinecap="square"
+                      trailWidth="2"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/Pages/Skills/utils.js
@@ -0,0 +1,24 @@
+export const skillsData = [
+  {
+    label: "FRONT END",
+    data: [
+      { skillName: "HTML", percentage: "90" },
+      { skillName: "CSS", percentage: "80" },
+
+      { skillName: "JavaScript", percentage: "70" },
+
+      { skillName: "React JS", percentage: "70" },
+
+      { skillName: "Redux", percentage: "50" },
+    ],
+  },
+
+  {
+    label: "Web Development Platform.",
+    data: [{ skillName: "Firebase Auth & Hosting", percentage: "70" }],
+  },
+  {
+    label: "DataBases",
+    data: [{ skillName: "Firebase Realtime DataBases", percentage: "50" }],
+  },
+];
