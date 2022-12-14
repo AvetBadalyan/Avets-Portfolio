72f28b3c9e135045881600ee56ba62c729b4651a
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-14 21:14:53 +0400

    feat: create database for website with screenshots, links and descriptions of websites

---
+++ src/Pages/Portfolio/portfolioData.js
@@ -0,0 +1,96 @@
+export const data = [
+  {
+    siteName: "AVET'S ONLINE FOOD STORE",
+    description:
+      "This is an online food store with a nice styling and principal functions - view meals menu with meal name, price and description, click on add button to add the meal to the cart, control the amount of chosen item, add or remove items to/from the cart. Also Authentication with Firebase - Sign up and login, make an order, send order data to server, get confirmation about receiving the order.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/foodStoreScreen2.jpg?alt=media&token=507e6c89-c6c0-4337-84c3-3591cfc9e8af",
+    webUrl: "https://online-foodstore.web.app/",
+    githubUrl: "https://github.com/AvetBadalyan/Online-Food-Store",
+    id: "WS01",
+  },
+  {
+    siteName: "ONLINE CLOTHES SHOP",
+    description:
+      "This is an online clothes store with principal functions - view items menu with each item's name, price and description, click on add button to add the product to the cart, control the amount of chosen item, add or remove items to/from the cart. Also Authentication with Firebase - Sign up and login",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/cart.jpg?alt=media&token=e1d56c03-4c98-4f9d-bfa4-a7231c28c63e",
+    webUrl: "https://avet-clothes-shop-f8267.web.app/",
+    githubUrl: "https://github.com/AvetBadalyan/Avet-Clothes-shop-with-Redux",
+    id: "WS02",
+  },
+  {
+    siteName: "Todo list with Redux toolkit",
+    description:
+      "This is an app due to which you can add your tasks to complete with dates, collect them in some special day and look them daily, edit already written task if you want to, also delete some or all of them, strike with line-through the completed ones by checking the box, clear all completed ones and so on. With Mobile adaptation and responsive design! This is some kind of exercise and practice of Redux Toolkit for me as a developer.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/todo-task-page.jpg?alt=media&token=13e59346-a6b0-4af8-8b5f-e0f5de331cf5",
+    webUrl: "https://todo-list-with-redux-toolkit.web.app/",
+    githubUrl:
+      "hhttps://github.com/AvetBadalyan/Todo-list-with-date-redux-toolkit",
+    id: "WS03",
+  },
+  {
+    siteName: "ITUNES MUSIC SEARCHER",
+    description:
+      "This is a little copy of apple music website where you can search songs, see the singer's name and image, click on the details link and see the song details, listen to a part of the song, see the price, duration, country and other details about the song.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/itunes.jpg?alt=media&token=bd165696-1d0b-4876-8f62-434421765821",
+    webUrl: "https://itunes-simple-clone-a7486.web.app/",
+    githubUrl: "https://github.com/AvetBadalyan/iTunes-music-Searcher",
+    id: "WS04",
+  },
+  {
+    siteName: "Joke Teller (Pure HTML/CSS, JS)",
+    description:
+      "This is a little app to click on the button and listen to a joke with subtitles. (used Text to speech engine with JavaScript DK) With Mobile adaptation",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/robot.jpg?alt=media&token=e4bf7c28-d1d2-49db-b9d4-d0ec1f123fa2",
+    webUrl: "https://get-joke-f9568.web.app/",
+    githubUrl:
+      "https://github.com/AvetBadalyan/Joke-Teller-with-Text-to-Speech",
+    id: "WS05",
+  },
+  {
+    siteName: "Quotes Generator (Pure HTML/CSS, JS)",
+    description:
+      "HERE IS A QUOTE GENERATOR! (a little app with Pure HTML/CSS and JS, Web and mobile responsive - data fetching and random generating)",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/quote%20home%20web.jpg?alt=media&token=c7866d82-c84b-49d0-8d72-062564a458c8",
+    webUrl: "https://quote-generator-c7fdb.web.app/",
+    githubUrl:
+      "https://github.com/AvetBadalyan/Quote-Generator-Pure-HTML-CSS-JS",
+    id: "WS06",
+  },
+  {
+    siteName: "PICTURE IN PICTURE MODE OF YOUTUBE (Pure HTML/CSS, JS)",
+    description:
+      "You click on button and you can see the video playing when you navigate from site to site",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/pic%20in%20pic%20home.jpg?alt=media&token=18bdf9ba-c6ed-462f-a8cd-3c590bd657fc",
+    webUrl: "https://picture-in-picture-mode.web.app/",
+    githubUrl: "https://github.com/AvetBadalyan/Picture-in-Picture-mode",
+    id: "WS07",
+  },
+  {
+    siteName: "Tales of Cocktails with Redux Toolkit",
+    description:
+      "You can see the whole collection of cocktails, search a single one, see details of a specific cocktail in another page. With Mobile Adaptation and Responsive design.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/cocktails.jpg?alt=media&token=d8582601-3a79-4039-9928-97d25ca83a1b",
+    webUrl: "https://tales-of-cocktails.web.app/",
+    githubUrl:
+      "https://github.com/AvetBadalyan/Tales-of-Cocktails-Redux-Toolkit",
+    id: "WS08",
+  },
+  {
+    siteName: "Paint it yourself!",
+    description:
+      "A small app where the screen is full of cells, you can paint them by clicking or dragging the mouse, also you can choose the color!",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/my-portfolio-f61ed.appspot.com/o/screenshot.jpg?alt=media&token=d29ed464-8e72-4aa3-bb07-72f8c12267d2",
+    webUrl: "https://paint-it-yourself.web.app/",
+    githubUrl: "https://github.com/AvetBadalyan/Paint-it-yourself",
+    id: "WS09",
+  },
+];
