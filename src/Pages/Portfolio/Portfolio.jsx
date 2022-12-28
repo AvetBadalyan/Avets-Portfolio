d894045920230f71220579235e37bb87a84da840
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-28 16:26:11 +0400

    feat: add loader spin

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -7,8 +7,10 @@ import "./Portfolio.styles.scss";
 export default function Portfolio() {
   const [data, setData] = useState([]);
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchData = useCallback(async () => {
+    setIsLoading(true);
     const response = await fetch(
       "https://my-portfolio-f61ed-default-rtdb.firebaseio.com/websites.json"
     );
@@ -16,7 +18,7 @@ export default function Portfolio() {
     const result = await response.json();
 
     setData(result);
-    console.log(result, "result");
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
@@ -27,45 +29,48 @@ export default function Portfolio() {
     <div className="portfolio-page">
       <PageHeader headerText="My Websites" />
       <div className="portfolio-content">
-        <div className="portfolio-content-cards">
-          {data.map((singleSite, index) => {
-            const { siteName, description, image, webUrl, githubUrl, id } =
-              singleSite;
-            return (
-              <div
-                onMouseEnter={() => setHoveredIndex(index)}
-                onMouseLeave={() => setHoveredIndex(null)}
-                key={id}
-                className="portfolio-content-cards-item"
-              >
-                <div className="portfolio-content-cards-item-img-wrapper">
-                  <img src={image} alt="project" />
-                </div>
-                <div className="overlay">
-                  {hoveredIndex === index && (
-                    <div className="site-info">
-                      <h1>{siteName}</h1>
-                      <p className="site-description">
-                        <span className="site-description-span">
-                          Description:
-                        </span>{" "}
-                        {description}
-                      </p>
-                      <div className="site-link-buttons">
-                        <a href={webUrl} target="_blank" rel="noreferrer">
-                          <button>Go to Website</button>
-                        </a>
-                        <a href={githubUrl} target="_blank" rel="noreferrer">
-                          <button>Code in GitHub</button>
-                        </a>
+        {isLoading && <div class="loader"></div>}
+        {!isLoading && (
+          <div className="portfolio-content-cards">
+            {data.map((singleSite, index) => {
+              const { siteName, description, image, webUrl, githubUrl, id } =
+                singleSite;
+              return (
+                <div
+                  onMouseEnter={() => setHoveredIndex(index)}
+                  onMouseLeave={() => setHoveredIndex(null)}
+                  key={id}
+                  className="portfolio-content-cards-item"
+                >
+                  <div className="portfolio-content-cards-item-img-wrapper">
+                    <img src={image} alt="project" />
+                  </div>
+                  <div className="overlay">
+                    {hoveredIndex === index && (
+                      <div className="site-info">
+                        <h1>{siteName}</h1>
+                        <p className="site-description">
+                          <span className="site-description-span">
+                            Description:
+                          </span>{" "}
+                          {description}
+                        </p>
+                        <div className="site-link-buttons">
+                          <a href={webUrl} target="_blank" rel="noreferrer">
+                            <button>Go to Website</button>
+                          </a>
+                          <a href={githubUrl} target="_blank" rel="noreferrer">
+                            <button>Code in GitHub</button>
+                          </a>
+                        </div>
                       </div>
-                    </div>
-                  )}
+                    )}
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
