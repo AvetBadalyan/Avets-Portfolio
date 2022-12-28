a713653f31d1a12419d6cd23d0e81dc6b2198798
AvetBadalyan <avetbadalyan@gmail.com>
2022-12-28 23:22:21 +0400

    feat: add logos to skills

--- src/Pages/Skills/Skills.jsx
+++ src/Pages/Skills/Skills.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PageHeader from "./../../Components/PageHeaderContent/PageHeader";
 import { skillsData } from "./utils";
-import { Line } from "rc-progress";
 import "./Skills.styles.scss";
 import { Animate } from "react-simple-animate";
 
@@ -13,31 +12,25 @@ export default function Skills() {
         <div className="skills-content-wrapper">
           {skillsData.map((item, key) => (
             <div key={key} className="skills-content-wrapper-inner-content">
-              <Animate
-                play
-                duration={1}
-                delay={0.4}
-                start={{ transform: "translateX(-250px)" }}
-                end={{ transform: "translateX(0px)" }}
-              >
-                <h3 className="skills-content-wrapper-inner-content-category-text">
-                  {item.label}
-                </h3>
-                <div>
-                  {item.data.map((dataItem, index) => (
-                    <div className="progressbar-wrapper" key={index}>
+              <h3 className="skills-content-wrapper-inner-content-category-text">
+                {item.label}
+              </h3>
+              <div className="skills-content-wrapper-logos">
+                {item.data.map((dataItem, index) => (
+                  <Animate
+                    play
+                    duration={1}
+                    delay={0.4}
+                    start={{ transform: "translateX(-250px)" }}
+                    end={{ transform: "translateX(0px)" }}
+                  >
+                    <div className="skill-logo-wrapper" key={index}>
                       <p>{dataItem.skillName}</p>
-                      <Line
-                        percent={dataItem.percentage}
-                        strokeWidth="2"
-                        strokeColor="var(--selected-theme-main-color)"
-                        strokeLinecap="square"
-                        trailWidth="2"
-                      />
+                      <img src={dataItem.logo} alt="logo" />
                     </div>
-                  ))}
-                </div>
-              </Animate>
+                  </Animate>
+                ))}
+              </div>
             </div>
           ))}
         </div>
--- src/Pages/Skills/utils.js
+++ src/Pages/Skills/utils.js
@@ -2,23 +2,47 @@ export const skillsData = [
   {
     label: "FRONT END",
     data: [
-      { skillName: "HTML", percentage: "90" },
-      { skillName: "CSS", percentage: "80" },
+      {
+        skillName: "HTML",
+        logo: "https://cdn.worldvectorlogo.com/logos/html-1.svg",
+      },
+      {
+        skillName: "CSS",
+        logo: "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+      },
 
-      { skillName: "JavaScript", percentage: "70" },
+      {
+        skillName: "JavaScript",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+      },
+      {
+        skillName: "React JS",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      },
 
-      { skillName: "React JS", percentage: "70" },
-
-      { skillName: "Redux", percentage: "50" },
+      {
+        skillName: "Redux",
+        logo: "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png",
+      },
     ],
   },
 
   {
-    label: "Platforms",
-    data: [{ skillName: "Firebase Auth & Hosting", percentage: "70" }],
-  },
-  {
-    label: "DataBases",
-    data: [{ skillName: "Firebase Realtime DB", percentage: "50" }],
+    label: "Other",
+    data: [
+      {
+        skillName: "Git",
+        logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png",
+      },
+      {
+        skillName: "GitHub",
+        logo: " https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg",
+      },
+
+      {
+        skillName: "Firebase",
+        logo: "https://png2.cleanpng.com/sh/9dfb3fd3d054d6891f805510bdf4a0d5/L0KzQYm3VMI3N5RBfZH0aYP2gLBuTfZqepZneeVuLXPvf8brTf1me6Rmf9t3Zz3qf7FujPUubJZ7fd54cHX1g376jBZ1f5J3RadqZUHnSbfrgcQ6PGEARqkCMEm0SYmAUcUzPGg6SKo9NUa6RIK1kP5o/kisspng-firebase-cloud-messaging-google-developers-softwar-5ae1d9fda49409.7709198715247508456741.png",
+      },
+    ],
   },
 ];
