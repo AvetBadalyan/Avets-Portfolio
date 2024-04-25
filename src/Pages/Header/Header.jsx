779a657e3c155b92b08cd1204e2f3100c9a5d7f6
AvetBadalyan <avetbadalyan@gmail.com>
2024-04-26 00:47:53 +0400

    update home structure

--- src/Pages/Header/Header.jsx
+++ src/Pages/Header/Header.jsx
@@ -16,40 +16,43 @@ const Header = () => {
       <h2 data-aos="flip-right" className="welcome-heading">
         Welcome To My Portfolio Website!
       </h2>
-      <div className="container welcome-container">
-        <div className="header__profile" data-aos="fade-in">
-          <img src={HeaderImage} alt="Header Portrait" />
-        </div>
-        <div data-aos="zoom-in-up" className="welcome-text">
+      <div className="container header-main-content">
+        <div className="header-left-part">
+          <div className="header__profile" data-aos="fade-in">
+            <img src={HeaderImage} alt="Header Portrait" />
+          </div>
           <h1 data-aos="zoom-in-up">Avet Badalyan</h1>
-          <h2 data-aos="zoom-in-up">Frontend Developer</h2>
-
-          <p className="home-main-text" data-aos="zoom-in-up">
-            I acquired fundamental Front-End Development knowledge through a
-            bootcamp at <strong>Armenian Code Academy</strong> and courses at{" "}
-            <strong>Microsoft IC Armenia</strong>. <br /> <br />
-            Then, at <strong>Cognaize Engineering LLC</strong>, I continued to
-            improve my skills by working on a variety of tasks, including
-            redesigning elements, debugging, and implementing new features.{" "}
-            <br />
-            Alongside this, I worked on personal projects and freelancing.
-            <br /> <br />
-            And now, with 2 years of hands-on experience, I'm excited to take on
-            new challenges and contribute to innovative projects!
-          </p>
         </div>
-      </div>
 
-      <div className="header__cta">
-        <a href="#contact" className="btn primary">
-          Let's Talk
-        </a>
-        <a href="#portfolio" className="btn primary">
-          Explore Projects
-        </a>
-        <a href={CV} download className="btn primary">
-          Download CV <FaDownload />
-        </a>
+        <div className=" welcome-container">
+          <h2 data-aos="zoom-in-up">Frontend Developer</h2>
+          <div data-aos="zoom-in-up" className="welcome-text">
+            <p className="home-main-text" data-aos="zoom-in-up">
+              I acquired fundamental Front-End Development knowledge through a
+              bootcamp at <strong>Armenian Code Academy</strong> and courses at
+              <strong>Microsoft IC Armenia</strong>. <br /> <br />
+              Then, at <strong>Cognaize Engineering LLC</strong>, I continued to
+              improve my skills by working on a variety of tasks, including
+              redesigning elements, debugging, and implementing new features.
+              <br />
+              Alongside this, I worked on personal projects and freelancing.
+              <br /> <br />
+              And now, with 2 years of hands-on experience, I'm excited to take
+              on new challenges and contribute to innovative projects!
+            </p>
+          </div>
+          <div className="header__buttons">
+            <a href="#contact" className="btn primary">
+              Let's Talk
+            </a>
+            <a href="#portfolio" className="btn primary">
+              Explore Projects
+            </a>
+            <a href={CV} download className="btn primary">
+              Download CV <FaDownload />
+            </a>
+          </div>
+        </div>
       </div>
     </header>
   );
