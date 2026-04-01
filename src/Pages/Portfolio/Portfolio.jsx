62188854b3703c44efe85b6107a0ed70e0f3eac3
AvetBadalyan <avetbadalyan@gmail.com>
2026-04-02 00:11:24 +0400

    images and db update to local

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,28 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Portfolio.scss";
 import ProjectsCategories from "./ProjectsCategories";
+import projectImages from "./projectImages";
+import portfolioData from "./portfolioData.json";
 
 const Portfolio = () => {
-  const [projects, setProjects] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(
-          "https://avets-portfolio-default-rtdb.firebaseio.com/websites.json"
-        );
-        const result = await response.json();
-        setProjects(result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+  const projects = (portfolioData.websites || []).map((project) => ({
+    ...project,
+    image: projectImages[project.imageFile] || project.imageFile,
+  }));
 
   const categories = projects.map((item) => item.category);
   const uniqueCategories = ["all", ...new Set(categories)];
@@ -32,19 +18,9 @@ const Portfolio = () => {
       <h2 data-aos="zoom-in-up" className="section-heading">
         Recent Projects
       </h2>
-      {isLoading && (
-        <div className="loader-container">
-          <div className="loader"></div>
-        </div>
-      )}
-      {!isLoading && (
-        <div className="container portfolio__container">
-          <ProjectsCategories
-            categories={uniqueCategories}
-            projects={projects}
-          />
-        </div>
-      )}
+      <div className="container portfolio__container">
+        <ProjectsCategories categories={uniqueCategories} projects={projects} />
+      </div>
     </section>
   );
 };
--- src/Pages/Skills/utils.js
+++ src/Pages/Skills/utils.js
@@ -1,58 +1,78 @@
+import htmlLogo from '../../assets/skills/html-1.svg';
+import cssLogo from '../../assets/skills/css-3.svg';
+import sassLogo from '../../assets/skills/sass-1.svg';
+import javascriptLogo from '../../assets/skills/logo-javascript.svg';
+import reactLogo from '../../assets/skills/React-icon.svg';
+import typescriptLogo from '../../assets/skills/typescript.svg';
+import reduxLogo from '../../assets/skills/redux.png';
+import bootstrapLogo from '../../assets/skills/bootstrap-4.svg';
+import responsiveLogo from '../../assets/skills/responsive.png';
+import shopifyLogo from '../../assets/skills/shopify-logo-png-transparent.png';
+import materialUiLogo from '../../assets/skills/material-ui-1.svg';
+import jiraLogo from '../../assets/skills/jira-1 (1).svg';
+import postmanLogo from '../../assets/skills/postman.svg';
+import firebaseLogo from '../../assets/skills/firebase-1.svg';
+import githubLogo from '../../assets/skills/Octicons-mark-github.svg';
+import gitLogo from '../../assets/skills/Git_icon.svg.png';
+import nodeLogo from '../../assets/skills/nodejs-1.svg';
+import expressLogo from '../../assets/skills/expressjs.jpg';
+import mongoLogo from '../../assets/skills/mongodb-icon-2.svg';
+
 export const frontendSkills = [
   {
     skillName: 'HTML',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fhtml-1.svg?alt=media&token=ee9228fd-67bf-482e-934b-3ec5ddbbc4dc',
+    logo: htmlLogo,
     percentage: 90,
   },
   {
     skillName: 'CSS',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fcss-3.svg?alt=media&token=9360bf91-c585-47b2-86aa-6d188684aaa3',
+    logo: cssLogo,
     percentage: 80,
   },
   {
     skillName: 'SASS',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fsass-1.svg?alt=media&token=9a2363a4-e9ff-4227-8c40-33a74de73ca6',
+    logo: sassLogo,
     percentage: 80,
   },
   {
     skillName: 'JavaScript',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Flogo-javascript.svg?alt=media&token=249253d4-aa1d-488b-b921-6e6e24db8e0b',
+    logo: javascriptLogo,
     percentage: 80,
   },
   {
     skillName: 'React.JS',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2FReact-icon.svg?alt=media&token=729f9108-e7a3-422e-9007-f56a629cc543',
+    logo: reactLogo,
     percentage: 80,
   },
   {
     skillName: 'TypeScript',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Ftypescript.svg?alt=media&token=b200b082-1ee4-4474-b894-38d6815bde1b',
+    logo: typescriptLogo,
     percentage: 60,
   },
   {
     skillName: 'Redux',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fredux.png?alt=media&token=065208e2-bd64-4ded-a4e3-afdc8410b260',
+    logo: reduxLogo,
     percentage: 60,
   },
   {
     skillName: 'Bootstrap',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fbootstrap-4.svg?alt=media&token=3dd2efb5-de65-4cbc-a958-4f1ca0b8bca3',
+    logo: bootstrapLogo,
     percentage: 60,
   },
 
   {
     skillName: 'Responsive WD',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fresponsive.png?alt=media&token=3829cb3e-8251-49ef-9553-04f562e519d4',
+    logo: responsiveLogo,
     percentage: 70,
   },
   {
     skillName: 'Shopify',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fshopify-logo-png-transparent.png?alt=media&token=0e24ceaf-af0d-4590-90a5-8b21847e286b',
+    logo: shopifyLogo,
     percentage: 60,
   },
   {
     skillName: 'Material UI',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fmaterial-ui-1.svg?alt=media&token=7e104321-016d-4379-8b1f-f09639028477',
+    logo: materialUiLogo,
     percentage: 20,
   },
 ];
@@ -60,43 +80,43 @@ export const frontendSkills = [
 export const backendSkills = [
   {
     skillName: 'Jira',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fjira-1%20(1).svg?alt=media&token=ee898c1f-92c5-497e-a454-6fc8b867d123',
+    logo: jiraLogo,
     percentage: 70,
   },
   {
     skillName: 'Postman',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fpostman.svg?alt=media&token=ab8ea31b-53c2-44d4-867a-893f5b4996a7',
+    logo: postmanLogo,
     percentage: 60,
   },
   {
     skillName: 'Firebase',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Ffirebase-1.svg?alt=media&token=39612fa5-4de2-4f64-b89d-b435e331e5c5',
+    logo: firebaseLogo,
     percentage: 50,
   },
   {
     skillName: 'GitHub',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2FOcticons-mark-github.svg?alt=media&token=70b35d25-a83d-4143-85f8-21af131824e4',
+    logo: githubLogo,
     percentage: 70,
   },
   {
     skillName: 'Git',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2FGit_icon.svg.png?alt=media&token=0bfc1aff-e47b-41f4-a4ee-26d9f9ffba14',
+    logo: gitLogo,
     percentage: 40,
   },
   {
     skillName: 'Node.JS',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fnodejs-1.svg?alt=media&token=8eb2fb68-fe05-42d7-82a6-0ce11579fa53',
+    logo: nodeLogo,
     percentage: 40,
   },
   {
     skillName: 'Express JS',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fexpressjs.jpg?alt=media&token=83bec0f1-9712-4d95-bfe2-f592e9ef85e5',
+    logo: expressLogo,
     percentage: 40,
   },
 
   {
     skillName: 'Mongo DB',
-    logo: 'https://firebasestorage.googleapis.com/v0/b/avets-portfolio.appspot.com/o/skills%2Fmongodb-icon-2.svg?alt=media&token=1780345e-7bdc-4a0b-a78d-e16656bb4056',
+    logo: mongoLogo,
     percentage: 40,
   },
 ];
